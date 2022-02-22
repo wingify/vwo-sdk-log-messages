290c8cb211d6fb17a6d47d60eccafd581d100112
Varun Malhotra <varun.malhotra@wingify.com>
2022-02-22 14:06:29 +0530

    fix path

--- src/VwoSdkLogMessages.php
+++ src/VwoSdkLogMessages.php
@@ -7,10 +7,10 @@ class VwoSdkLogMessages {
   public static function get()
   {
     return [
-      "debugLogs" => json_decode(file_get_contents(__DIR__ . "debug-messages.json"), true),
-      "infoLogs" => json_decode(file_get_contents(__DIR__ . "info-messages.json"), true),
-      "errorLogs" => json_decode(file_get_contents(__DIR__ . "error-messages.json"), true),
-      "warnLogs" => json_decode(file_get_contents(__DIR__ . "warning-messages.json"), true)
+      "debugLogs" => json_decode(file_get_contents(__DIR__ . "/debug-messages.json"), true),
+      "infoLogs" => json_decode(file_get_contents(__DIR__ . "/info-messages.json"), true),
+      "errorLogs" => json_decode(file_get_contents(__DIR__ . "/error-messages.json"), true),
+      "warnLogs" => json_decode(file_get_contents(__DIR__ . "/warning-messages.json"), true)
     ];
   }
 }
