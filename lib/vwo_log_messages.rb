dbbee32957a9c523c963abda880b303bbd051407
softvar <varun2902@gmail.com>
2022-02-28 14:06:19 +0530

    update ruby file

--- lib/vwo_log_messages.rb
+++ lib/vwo_log_messages.rb
@@ -1,8 +1,10 @@
 class VwoLogMessages
-  # def get
-  #   # puts JSON.load(File.open(File.join(File.dirname(__FILE__), '/debug-messages.json')))
-  # end
   def self.getMessage
-    puts "Hello world!"
+      return {
+          'debug_logs' => JSON.load(File.open(File.join(File.dirname(__FILE__), '../src/debug-messages.json'))),
+          'error_logs' => JSON.load(File.open(File.join(File.dirname(__FILE__), '../src/error-messages.json'))),
+          'info_logs' => JSON.load(File.open(File.join(File.dirname(__FILE__), '../src/info-messages.json'))),
+          'warning_logs' => JSON.load(File.open(File.join(File.dirname(__FILE__), '../src/warning-messages.json')))
+      }
   end
 end
